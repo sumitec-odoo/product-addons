a9aee9a85be3513ca2ccbfb777bedb5b60b9b480
Juan Pablo Garza <juanp@juanpgarza.com>
2023-02-19 11:32:23 -0300

    [MIG] product_category_group

--- product_category_group/__manifest__.py
+++ product_category_group/__manifest__.py
@@ -4,16 +4,19 @@
 {
     "name": "Product Category Group",
     "summary": "",
-    "version": "12.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Product",
     "website": "https://github.com/juanpgarza/product-addons",
     "author": "juanpgarza",
     "license": "AGPL-3",
-    "depends": ["product","stock"],
+    "depends": [
+                "product",
+                "stock",
+            ],
     "data": [
         'views/product_category_group_views.xml',
         'views/product_category_views.xml',        
         'security/ir.model.access.csv',        
         ],
-    "installable": False,
+    "installable": True,
 }
